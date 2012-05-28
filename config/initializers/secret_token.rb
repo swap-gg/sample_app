761ca50abc456dc26052713639a5a0a7ebca7a15
Swapnil <swapnil@groupgyaan.com>
2012-05-28 22:25:31 +0530

    Initial commit of sample app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '3c1d98465a0253da688bfd4ce7a6e46f59ce0731c9f3b134f1c943621403038dde5b20d8fa7f6604e9ab2206cc5d1c44f75ec9f8a907ba42b47833b855948fb6'
